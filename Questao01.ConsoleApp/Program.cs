70e1790bed0371427bc0090cb8404bef6b2835fe
Dudu <duducardoso2701@gmail.com>
2025-02-04 16:04:59 -0300

    Add project files.

---
+++ Questao01.ConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Informe um número: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        if (PertenceFibonacci(numero))
+        {
+            Console.WriteLine($"O número {numero} pertence à sequência de Fibonacci.");
+        }
+        else
+        {
+            Console.WriteLine($"O número {numero} NÃO pertence à sequência de Fibonacci.");
+        }
+    }
+
+    static bool PertenceFibonacci(int n)
+    {
+        int a = 0, b = 1, temp;
+
+        while (a <= n)
+        {
+            if (a == n)
+                return true;
+
+            temp = a + b;
+            a = b;
+            b = temp;
+        }
+
+        return false;
+    }
+}
